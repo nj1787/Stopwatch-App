c5a9f28ec9db919d837c4c54169aae574997880f
nj1787 <neerajj9@gmail.com>
2023-06-01 20:23:29 +0530

    Development Complted.

---
+++ Javascript/script.js
@@ -0,0 +1,82 @@
+let hours = 0;
+let minutes = 0;
+let seconds = 0;
+let timer;
+
+// Get elements from the DOM
+const display = document.getElementById("timeTracker");
+const logDisplay = document.getElementById("timelog-display");
+const startTimer = document.getElementById("start");
+const stopTimer = document.getElementById("stop");
+const resetTimer = document.getElementById("reset");
+const logTimer = document.getElementById("log");
+
+// Start/stop button event listener
+startTimer.addEventListener("click", function () {
+  start();
+});
+
+stopTimer.addEventListener("click", function () {
+  stop();
+});
+
+// Reset button event listener
+resetTimer.addEventListener("click", function () {
+  reset();
+});
+
+logTimer.addEventListener("click", function () {
+  timeLog();
+});
+
+// Start function
+function start() {
+  document.body.style.backgroundColor = "green";
+  timer = setInterval(function () {
+    seconds++;
+
+    if (seconds === 60) {
+      seconds = 0;
+      minutes++;
+
+      if (minutes === 60) {
+        minutes = 0;
+        hours++;
+      }
+    }
+
+    display.textContent =
+      (hours ? (hours > 9 ? hours : "0" + hours) : "00") +
+      ":" +
+      (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") +
+      ":" +
+      (seconds > 9 ? seconds : "0" + seconds);
+  }, 1000);
+}
+
+// Stop function
+function stop() {
+  document.body.style.backgroundColor = "red";
+  clearInterval(timer);
+}
+
+// Reset function
+function reset() {
+  document.body.style.backgroundColor = "white";
+  clearInterval(timer);
+  hours = 0;
+  minutes = 0;
+  seconds = 0;
+  display.textContent = "00:00:00";
+  logDisplay.innerHTML = `<h1>TimeLog Display</h1>`;
+}
+
+//Timelog
+function timeLog() {
+  const list = document.createElement("ul");
+  list.style.marginLeft = "16%";
+  list.style.fontSize = "1rem";
+  list.style.marginTop = "12%";
+  list.innerHTML = `<li style="margin-top: none">Logged At ${hours}:${minutes}:${seconds}</li>`;
+  logDisplay.append(list);
+}
